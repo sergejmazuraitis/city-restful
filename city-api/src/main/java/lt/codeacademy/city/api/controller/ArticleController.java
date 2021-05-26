ae12241a6caf48dd4de3b45948b9c98359f27422
Sergej Mazuraitis <sergejmazuraitis@gmail.com>
2021-05-26 12:00:08 +0300

    create Article controller

--- city-api/src/main/java/lt/codeacademy/city/api/controller/ArticleController.java
+++ city-api/src/main/java/lt/codeacademy/city/api/controller/ArticleController.java
@@ -1,8 +1,32 @@
 package lt.codeacademy.city.api.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lt.codeacademy.city.api.Endpoint;
+import lt.codeacademy.city.api.entity.Article;
+import lt.codeacademy.city.api.service.ArticleService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
-
+@RequestMapping(Endpoint.API_ROOT + Endpoint.ARTICLES)
 public class ArticleController {
+
+    private final ArticleService articleService;
+
+    public ArticleController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addArticle(@Valid @RequestBody Article article) {
+        articleService.addArticle(article);
+    }
+
+    @GetMapping
+    public List<Article> getAllArticles(){
+        return articleService.getAllArticles();
+    }
 }
