1b4febd9b15d82367693b8559158955f3b681c29
Sergej Mazuraitis <sergejmazuraitis@gmail.com>
2021-05-26 15:10:13 +0300

    init React App add Footer and Header

---
+++ city-ui/city-ui/src/components/Header/Header.js
@@ -0,0 +1,63 @@
+import React, {useContext} from "react";
+import AppBar from "@material-ui/core/AppBar";
+import {Badge, Toolbar} from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+import Button from "@material-ui/core/Button";
+import {makeStyles} from "@material-ui/core/styles";
+import {NavLink} from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+    appBar: {
+        borderBottom: `1px solid ${theme.palette.divider}`,
+    },
+    toolbar: {
+        flexWrap: 'wrap',
+    },
+    toolbarTitle: {
+        flexGrow: 1,
+    },
+    link: {
+        margin: theme.spacing(1, 1.5),
+    },
+    active: {
+        fontWeight: "bolder",
+        color: "blue"
+    }
+}));
+
+export default () => {
+    const classes = useStyles();
+
+    return (
+        <React.Fragment>
+            <AppBar position="static" color="default" elevation={0} className={classes.appBar}>
+                <Toolbar className={classes.toolbar}>
+                    <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
+                        Eshop
+                    </Typography>
+                    <nav>
+                        <Link   variant="button" color="textPrimary" to="/signup"
+                                className={classes.link} activeClassName={classes.active} component={NavLink}>
+                            Registration
+                        </Link>
+                        <Link   variant="button" color="textPrimary" to="/products"
+                              className={classes.link} activeClassName={classes.active} component={NavLink}>
+                            Products
+                        </Link>
+                        <Link variant="button" color="textPrimary" href="#" className={classes.link}>
+                            Features
+                        </Link>
+                        <Link variant="button" color="textPrimary" href="#" className={classes.link}>
+                            Users
+                        </Link>
+                    </nav>
+                    <Button href="#" color="primary" variant="outlined" className={classes.link}>
+                        Login
+                    </Button>
+                </Toolbar>
+            </AppBar>
+
+        </React.Fragment>
+    )
+}
