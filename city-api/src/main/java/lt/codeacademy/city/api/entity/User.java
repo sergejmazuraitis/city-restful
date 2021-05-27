2ff80d37311459526f28015cfcba80bd48fb4589
Sergej Mazuraitis <sergejmazuraitis@gmail.com>
2021-05-27 13:34:53 +0300

    add User entity

---
+++ city-api/src/main/java/lt/codeacademy/city/api/entity/User.java
@@ -0,0 +1,48 @@
+package lt.codeacademy.city.api.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.util.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "Users")
+public class User {
+    @Id
+    @GeneratedValue
+    @Column(columnDefinition = "VARCHAR(36)", updatable = false)
+    @Type(type = "uuid-char")
+    private UUID userId;
+
+    @NotNull
+    @Size(min = 2, max = 250)
+    private String name;
+
+    @NotNull
+    @Size(min = 3, max = 250)
+    private String surname;
+
+    @NotNull
+    @Size(min = 1, max = 250)
+    @Column(unique = true)
+    private String username;
+
+    @NotNull
+    private String phone;
+
+    @NotNull
+    @Email
+    private String email;
+
+    @NotNull
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d).{8,}$")
+    private String password;
+}
