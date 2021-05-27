5b3ab9a4346b57f2a2bf479187d3a1797b64761c
Sergej Mazuraitis <sergejmazuraitis@gmail.com>
2021-05-27 14:02:25 +0300

    add more functions to ArticleService

--- city-api/src/main/java/lt/codeacademy/city/api/service/ArticleService.java
+++ city-api/src/main/java/lt/codeacademy/city/api/service/ArticleService.java
@@ -2,10 +2,12 @@ package lt.codeacademy.city.api.service;
 
 import lombok.extern.slf4j.Slf4j;
 import lt.codeacademy.city.api.entity.Article;
+import lt.codeacademy.city.api.exception.ArticleNotFoundException;
 import lt.codeacademy.city.api.repository.ArticleRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @Slf4j
@@ -26,5 +28,21 @@ public class ArticleService {
         }catch (IllegalArgumentException e) {
             log.error("Cannot create article {}", article);
         }
+    }
+
+    public Article getArticle(UUID id) {
+        return articleRepository.findById(id).orElseThrow(() -> new ArticleNotFoundException(id.toString()));
+    }
+
+    public void updateArticle(Article article) {
+        try {
+            articleRepository.save(article);
+        }catch (IllegalArgumentException e) {
+            log.error("Cannot create article {}", article);
+        }
+    }
+
+    public void deleteArticle(UUID id){
+        articleRepository.deleteById(id);
     }
 }
