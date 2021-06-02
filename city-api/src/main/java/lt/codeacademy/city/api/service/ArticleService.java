06ee66e0d3b51ab8ebbd459764c2faef06aec857
Sergej Mazuraitis <sergejmazuraitis@gmail.com>
2021-06-02 11:03:49 +0300

    add File upload methods to ArticleService

--- city-api/src/main/java/lt/codeacademy/city/api/service/ArticleService.java
+++ city-api/src/main/java/lt/codeacademy/city/api/service/ArticleService.java
@@ -2,20 +2,40 @@ package lt.codeacademy.city.api.service;
 
 import lombok.extern.slf4j.Slf4j;
 import lt.codeacademy.city.api.entity.Article;
+import lt.codeacademy.city.api.entity.Image;
 import lt.codeacademy.city.api.exception.ArticleNotFoundException;
+import lt.codeacademy.city.api.exception.ImageException;
 import lt.codeacademy.city.api.repository.ArticleRepository;
+import lt.codeacademy.city.api.repository.ImageRepository;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 @Service
 @Slf4j
 public class ArticleService {
-    private final ArticleRepository articleRepository;
+    private static final int MAX_SIZE = 10000000; //10MB
+    private final Set<String> types;
+    private final Path fileLocation;
 
-    public ArticleService(ArticleRepository articleRepository) {
+    private final ArticleRepository articleRepository;
+    private final ImageRepository imageRepository;
+
+    public ArticleService(ArticleRepository articleRepository, ImageRepository imageRepository) {
         this.articleRepository = articleRepository;
+        this.imageRepository = imageRepository;
+        fileLocation = Paths.get("./files").toAbsolutePath().normalize();
+        types = Set.of(MediaType.IMAGE_GIF_VALUE, MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE);
+
     }
 
     public List<Article> getAllArticles() {
@@ -45,4 +65,62 @@ public class ArticleService {
     public void deleteArticle(UUID id){
         articleRepository.deleteById(id);
     }
+
+    public void saveNewArticle(MultipartFile multipartFile, String name, String description, String content) throws IOException {
+        Article article = new Article();
+        Image image = new Image();
+        image.setFileName(multipartFile.getName());
+        image.setMediaType(multipartFile.getContentType());
+//        image.setBytes(multipartFile.getBytes());
+        image.setSize(multipartFile.getSize());
+
+        article.setName(name);
+        article.setDescription(description);
+        article.setContent(content);
+//        article.setImage(image);
+
+        articleRepository.save(article);
+    }
+
+    public void saveImageInFileSystem(MultipartFile multipartFile) {
+        validateFile(multipartFile);
+        createDirectory();
+
+        try {
+            Image image = new Image();
+            image.setFileName(multipartFile.getOriginalFilename());
+            image.setMediaType(multipartFile.getContentType());
+            image.setSize(multipartFile.getSize());
+
+           image = imageRepository.save(image);
+
+            Path newFilePath = fileLocation.resolve(image.getId().toString());
+            Files.copy(multipartFile.getInputStream(), newFilePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (Exception e) {
+            log.error("Cannot create file ", e);
+            throw new ImageException("Cannot create file");
+        }
+    }
+
+    private void createDirectory() {
+        try {
+            if (!Files.exists(fileLocation)) {
+                Files.createDirectory(fileLocation);
+            }
+        } catch (Exception e) {
+            log.error("Cannot create directory ", e);
+            throw new ImageException("Cannot create directory");
+        }
+    }
+
+    private void validateFile(MultipartFile file) {
+        if (file.getSize() > MAX_SIZE) {
+            throw new ImageException("Image is to big");
+        }
+
+        if (!types.contains(file.getContentType())) {
+            throw new ImageException("Image media type not allowed");
+        }
+    }
+
 }
