13ea7db78dee8666010b77c1099964a59ac1366a
Sergej Mazuraitis <sergejmazuraitis@gmail.com>
2021-06-02 11:05:47 +0300

    add Post methods with MultipartFile to ArticleController

--- city-api/src/main/java/lt/codeacademy/city/api/controller/ArticleController.java
+++ city-api/src/main/java/lt/codeacademy/city/api/controller/ArticleController.java
@@ -5,8 +5,10 @@ import lt.codeacademy.city.api.entity.Article;
 import lt.codeacademy.city.api.service.ArticleService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
@@ -20,11 +22,11 @@ public class ArticleController {
         this.articleService = articleService;
     }
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public void addArticle(@Valid @RequestBody Article article) {
-        articleService.addArticle(article);
-    }
+//    @PostMapping
+//    @ResponseStatus(HttpStatus.CREATED)
+//    public void addArticle(@Valid @RequestBody Article article) {
+//        articleService.addArticle(article);
+//    }
 
     @GetMapping
     public List<Article> getAllArticles() {
@@ -46,5 +48,23 @@ public class ArticleController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteArticle(@PathVariable(Endpoint.UUID) UUID uuid) {
         articleService.deleteArticle(uuid);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveArticleWithImage(@RequestParam MultipartFile multipartFile,
+                                     @RequestParam String name,
+                                     @RequestParam String description,
+                                     @RequestParam String content) throws IOException {
+        articleService.saveNewArticle(multipartFile, name, description, content);
+    }
+
+    @PostMapping("/upload")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveImagenFileSystem(@RequestParam MultipartFile multipartFile,
+                                     @RequestParam String name,
+                                     @RequestParam String description,
+                                     @RequestParam String content) {
+        articleService.saveImageInFileSystem(multipartFile);
     }
 }
