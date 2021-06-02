b3e3cb2b17c128205a1222e685218a9ee42de8f0
Sergej Mazuraitis <sergejmazuraitis@gmail.com>
2021-06-02 16:34:41 +0300

    fix paths in controller

--- city-api/src/main/java/lt/codeacademy/city/api/controller/ArticleController.java
+++ city-api/src/main/java/lt/codeacademy/city/api/controller/ArticleController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping(Endpoint.API_ROOT + Endpoint.ARTICLES)
+@RequestMapping(Endpoint.ARTICLES)
 public class ArticleController {
 
     private final ArticleService articleService;
