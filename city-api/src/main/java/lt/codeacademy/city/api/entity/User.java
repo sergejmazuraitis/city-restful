62d0778a7c3b73238c8051b5906003e4688dab93
Sergej Mazuraitis <sergejmazuraitis@gmail.com>
2021-06-02 12:09:26 +0300

    implement in User UserDetails

--- city-api/src/main/java/lt/codeacademy/city/api/entity/User.java
+++ city-api/src/main/java/lt/codeacademy/city/api/entity/User.java
@@ -3,6 +3,8 @@ package lt.codeacademy.city.api.entity;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.Type;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
@@ -15,7 +17,7 @@ import java.util.*;
 @Setter
 @Entity
 @Table(name = "Users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue
     @Column(columnDefinition = "VARCHAR(36)", updatable = false)
@@ -48,4 +50,29 @@ public class User {
 
     @ManyToMany
     private Set<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
