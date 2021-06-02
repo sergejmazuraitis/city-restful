9afab8cd3680668b1edd65b9079aa84e9f58139f
Sergej Mazuraitis <sergejmazuraitis@gmail.com>
2021-06-02 12:44:15 +0300

    create JwtAuthenticationFilter

---
+++ city-api/src/main/java/lt/codeacademy/city/api/security/JwtAuthenticationFilter.java
@@ -0,0 +1,53 @@
+package lt.codeacademy.city.api.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lt.codeacademy.city.api.entity.User;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final ObjectMapper objectMapper;
+    private final JwtService jwtService;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager,
+                                   ObjectMapper objectMapper,
+                                   JwtService jwtService) {
+        super(authenticationManager);
+        this.objectMapper = objectMapper;
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request,
+                                                HttpServletResponse response)
+            throws AuthenticationException {
+
+        try {
+            LoginDto loginDto = objectMapper.readValue(request.getReader(), LoginDto.class);
+            UsernamePasswordAuthenticationToken authRequest =
+                    new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
+
+            return getAuthenticationManager().authenticate(authRequest);
+        }catch (IOException e) {
+            throw new BadCredentialsException("Unable to parse.");
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) {
+        response.addHeader("Authorization", jwtService.createToken((User) authResult.getPrincipal()));
+    }
+}
