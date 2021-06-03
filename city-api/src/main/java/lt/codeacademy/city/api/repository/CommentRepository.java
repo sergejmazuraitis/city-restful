83c1a0b1d682b31ec6dcbdc4b3b07f95ad84efe5
Sergej Mazuraitis <sergejmazuraitis@gmail.com>
2021-06-03 15:14:46 +0300

    create CommentService

--- city-api/src/main/java/lt/codeacademy/city/api/repository/CommentRepository.java
+++ city-api/src/main/java/lt/codeacademy/city/api/repository/CommentRepository.java
@@ -3,7 +3,9 @@ package lt.codeacademy.city.api.repository;
 import lt.codeacademy.city.api.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CommentRepository extends JpaRepository<Comment, UUID> {
+    List<Comment> findAllById(UUID id);
 }
