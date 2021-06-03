f0f6c8e6e79a5dbbb35767c9f0a2a6f5ae43113b
Sergej Mazuraitis <sergejmazuraitis@gmail.com>
2021-06-03 10:45:00 +0300

    create lt and en translations for Header

--- city-ui/city-ui/src/i18n/i18n.js
+++ city-ui/city-ui/src/i18n/i18n.js
@@ -1,18 +1,20 @@
 import i18n from "i18next";
 import {initReactI18next} from "react-i18next";
 import languageDetector from "i18next-browser-languagedetector";
+import en from "./translations/en.json";
+import lt from "./translations/lt.json";
 
 i18n
     .use(initReactI18next)
     .use(languageDetector)
     .init({
         resources: {
-            en
+            en, lt
         },
         load: 'languageOnly',
         ns: [],
         fallbackLng: "en",
-        whitelist: ["en"],
+        whitelist: ["en", "lt"],
         interpolation: {
             escapeValue: false
         }
