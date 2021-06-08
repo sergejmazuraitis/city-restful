fbecbec33f58493caf948f6c5c6cdcf01364e410
Sergej Mazuraitis <sergejmazuraitis@gmail.com>
2021-06-08 11:14:28 +0300

    create constructStore and put in user

--- city-ui/city-ui/src/store/index.js
+++ city-ui/city-ui/src/store/index.js
@@ -1,11 +1,14 @@
 import {configureStore} from "@reduxjs/toolkit";
 import {logger} from "redux-logger/src";
+import user from "./slices/userSllice";
 
 
 const constructStore = () => {
 
     const store = configureStore({
-        reducer: {},
+        reducer: {
+            user
+        },
         middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
         preloadedState: {}
     })
