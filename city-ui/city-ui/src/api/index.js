2f9327283caf42b726a334cf76feda5b34f04adb
Sergej Mazuraitis <sergejmazuraitis@gmail.com>
2021-06-08 11:54:05 +0300

    get jwt in Axios interceptors

--- city-ui/city-ui/src/api/index.js
+++ city-ui/city-ui/src/api/index.js
@@ -1,7 +1,15 @@
 import axios from "axios"
+import store from "../store";
 
 const HTTP = axios.create({
     baseURL: '/city/api'
+})
+
+HTTP.interceptors.request.use(config => {
+    if (store.getState().user.jwt) {
+        config.headers.authorization = "Bearer " + store.getState().user.jwt
+    }
+    return config
 })
 
 export default HTTP
