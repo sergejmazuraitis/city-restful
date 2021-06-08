8d1963810fef9e352cf76db43e3f168f60168742
Sergej Mazuraitis <sergejmazuraitis@gmail.com>
2021-06-08 16:54:25 +0300

    add id in LoginUserDto

--- city-api/src/main/java/lt/codeacademy/city/api/dto/LoginUserDto.java
+++ city-api/src/main/java/lt/codeacademy/city/api/dto/LoginUserDto.java
@@ -5,16 +5,19 @@ import lt.codeacademy.city.api.entity.Role;
 import lt.codeacademy.city.api.entity.User;
 
 import java.util.Set;
+import java.util.UUID;
 
 import static java.util.stream.Collectors.toSet;
 
 @Data
 public class LoginUserDto {
+    UUID id;
     String username;
     String fullName;
     Set<String> roles;
 
     public LoginUserDto(User user) {
+        this.id = user.getId();
         this.username = user.getUsername();
         this.fullName = user.getName() + " " + user.getSurname();
         this.roles = user.getRoles().stream()
