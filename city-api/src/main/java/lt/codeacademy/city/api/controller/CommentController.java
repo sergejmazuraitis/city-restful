82155047ea6642e674c2d852d7a6164a8b21d52b
Sergej Mazuraitis <sergejmazuraitis@gmail.com>
2021-06-12 19:14:11 +0300

    add in CommentController preauthorized

--- city-api/src/main/java/lt/codeacademy/city/api/controller/CommentController.java
+++ city-api/src/main/java/lt/codeacademy/city/api/controller/CommentController.java
@@ -4,6 +4,7 @@ import lt.codeacademy.city.api.Endpoint;
 import lt.codeacademy.city.api.entity.Comment;
 import lt.codeacademy.city.api.service.CommentService;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -27,23 +28,25 @@ public class CommentController {
 
     @GetMapping(Endpoint.COMMENT)
     public Comment getCommentById(@PathVariable(Endpoint.UUID) UUID uuid) {
-        Comment c = commentService.getCommentById(uuid);
-        return c;
+        return commentService.getCommentById(uuid);
     }
 
     @DeleteMapping(Endpoint.COMMENT)
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     public void deleteComment(@PathVariable(Endpoint.UUID) UUID uuid) {
         commentService.deleteComment(uuid);
     }
 
     @PostMapping(Endpoint.NEW_COMMENT)
     @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     public void creteNewComment(@Valid @RequestBody Comment comment) {
         commentService.addComment(comment);
     }
 
     @PutMapping(Endpoint.NEW_COMMENT)
     @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     public void updateComment(@Valid @RequestBody Comment comment) {
         commentService.updateComment(comment);
     }
