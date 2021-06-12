1e383d7b2e23165adb8096cb8a8f74126dc4ff31
Sergej Mazuraitis <sergejmazuraitis@gmail.com>
2021-06-12 19:16:33 +0300

    fix new article create page (change file input and onSubmit)

--- city-ui/city-ui/src/pages/NewArticlePage/NewArticlePage.js
+++ city-ui/city-ui/src/pages/NewArticlePage/NewArticlePage.js
@@ -1,12 +1,127 @@
-import NewArticle from "../../components/NewArticle/NewArticle"
+import {Form, Formik} from "formik";
+import * as Yup from "yup";
+import Container from "@material-ui/core/Container";
+import {Paper, Typography} from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import {addNewArticleWithImage} from "../../api/articlesApi";
+import FormikInput from "../../components/FormikInput/FormikInput";
+import React, {useState} from "react";
+import {useTranslation} from "react-i18next";
+import {useHistory} from "react-router-dom";
+
+const validationSchema = Yup.object().shape({
+    name: Yup.string()
+        .required(),
+    description: Yup.string()
+        .required(),
+    content: Yup.string()
+        .required()
+})
 
 const NewArticlePage = () => {
+    const {t} = useTranslation('newArticle');
+    const [imageName, setImageName] = useState();
+    const history = useHistory();
+    const initialValues = {
+        name: '',
+        description: '',
+        content: '',
+        image: null
+    }
+    const FileUpload = () => {
+        document.getElementById('file').click();
+    }
+
+    const postNewArticle = (newArticle, {setSubmitting, resetForm}) => {
+        setSubmitting(true)
+
+        addNewArticleWithImage(newArticle)
+            .then(() => {
+                resetForm(initialValues)
+                history.push('/articles');
+            })
+            .finally(() => setSubmitting(false))
+
+    }
+
 
     return (
-        <>
-            <NewArticle/>
-        </>
+        <Formik initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={postNewArticle}>
+            {
+                props => (
+                    <>
+                        <Container maxWidth={"md"} style={{margin: "auto"}}>
+                            <Paper elevation={3} style={{margin: "auto", padding: "15px 0"}}>
+                                <Form style={{margin: "10px 40px", textAlign: "center"}}>
+                                    <Typography color="textPrimary"
+                                                align="center"
+                                                variant="h4"
+                                                paragraph
+                                    >
+                                        {t('NewArticle')}
+                                    </Typography>
 
+                                    <div>
+                                        <FormikInput name="name"
+                                                     label={t("ArticleName")}
+                                                     error={props.touched.name && !!props.errors.name}/>
+                                    </div>
+                                    <div>
+                                        <FormikInput name="description"
+                                                     label={t("Description")}
+                                                     error={props.touched.description && !!props.errors.description}/>
+                                    </div>
+                                    <div>
+                                        <FormikInput name="content"
+                                                     label={t("Content")}
+                                                     multiline rows={10}
+                                                     error={props.touched.content && !!props.errors.content}/>
+                                    </div>
+
+                                    <div style={{margin: "5px 0", textAlign: "left"}}>
+                                        <input id="file"
+                                               name="image"
+                                               type="file"
+                                               style={{
+                                                   display: "none"
+                                               }}
+                                               onChange={(event) => {
+                                                   setImageName(event.currentTarget.files[0].name);
+                                                   props.setFieldValue("image", event.currentTarget.files[0]);
+
+                                               }}
+                                               className="form-control"/>
+                                        <Button variant="outlined"
+                                                onClick={() => FileUpload()}>
+                                            Chosse file here
+                                        </Button>
+                                        {!imageName ?
+                                            <span>No file chosen, yet</span>
+                                            :
+                                            <span>{imageName}</span>
+                                        }
+                                    </div>
+
+                                    {!props.isSubmitting ?
+                                        <Button variant="contained"
+                                                type="submit"
+                                                style={{
+                                                    marginTop: "10px"
+                                                }}
+                                                disabled={props.isSubmitting}>
+                                            Submit
+                                        </Button>
+                                        :
+                                        <span>Submitting...</span>}
+                                </Form>
+                            </Paper>
+                        </Container>
+                    </>
+                )
+            }
+        </Formik>
     )
 }
 
