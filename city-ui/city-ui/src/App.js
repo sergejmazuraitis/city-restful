fad802ed26ef4ea82fecdfcd010e74145e4ba654
Sergej Mazuraitis <sergejmazuraitis@gmail.com>
2021-06-15 20:16:01 +0300

    commit files wich forgot commited

---
+++ city-ui/city-ui/src/App.js
@@ -0,0 +1,44 @@
+import CssBaseline from "@material-ui/core/CssBaseline";
+import {BrowserRouter as Router} from "react-router-dom";
+import {makeStyles} from "@material-ui/core";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Content from "./components/Content/index"
+import {Provider} from "react-redux";
+import store from "./store";
+import cityBack from "./img/city-back.jpg";
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+        backgroundImage: `url(${cityBack})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundAttachment: 'fixed',
+    },
+}));
+
+function App() {
+
+    const classes = useStyles();
+
+    return (
+        <Provider store={store}>
+
+            <Router>
+                <div className={classes.root}>
+                    <CssBaseline/>
+                    <Header/>
+                    <Content/>
+                    <Footer/>
+                </div>
+            </Router>
+        </Provider>
+    );
+}
+
+export default App;
